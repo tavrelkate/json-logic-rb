09f78dff6086e3953936bb6c6ddf20685ab7d407
kate <kate@MacBook-Air-kate.local>
2026-02-17 19:49:26 +0300

    Update throw.rb syntax.

--- lib/json_logic/operations/throw.rb
+++ lib/json_logic/operations/throw.rb
@@ -5,5 +5,8 @@ using JsonLogic::Semantics
 class JsonLogic::Operations::Throw < JsonLogic::Operation
   def self.name = "throw"
 
-  def call((value), _data) = raise JsonLogic::LogicError, (value.is_a?(Hash) ? value : { "type" => value.to_s })
+  def call((value), _data)
+    payload = value.is_a?(Hash) ? value : { "type" => value.to_s }
+    raise JsonLogic::LogicError, payload
+  end
 end
